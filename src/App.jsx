bfdd8e621c0588cd2d59e25ecc672734307e3bd6
Adil Saju <adilsaju@gmail.com>
2023-02-19 23:45:09 -0800

    2nd

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import Blog from './components/Blog'
+
+function App() {
+  return (
+    <div className="App">
+        <nav>
+        <ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+
+        </nav>
+
+        <div className='blogs'>
+            <Blog></Blog>
+        </div>
+
+    </div>
+  );
+}
+
+export default App;
