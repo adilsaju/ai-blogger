f834c2b7d1899452c01f34b23bfc5690b00d260c
Adil Saju <adilsaju@gmail.com>
2023-02-21 12:41:10 -0800

    all done

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -53,6 +53,44 @@ router.get('/questions', async function(req, res, next) {
 });
 
 
+router.post('/question', async function(req, res, next) {
+  //call open ai
+  let d1 = await  fetch("https://api.openai.com/v1/completions", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${config.apiKey}`,
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: "Can you write an easy random mathematics numerical challenge for me with answers in one number after Answer :",
+      max_tokens: 102,
+      temperature: 1,
+    }),
+  })
+  let d2 = await d1.json()
+  console.log(d2);
+  // console.log(d2['choices'][0]['text']);
 
+//questionnaire
+  let searchString = "Answer";
+  let index = d2.choices[0].text.indexOf(searchString);
+  if (index !== -1) {
+    let valueAfterSearchString2 = d2.choices[0].text.substring(0,index);
+    
+    // setContent(valueAfterSearchString2)
+
+    let valueAfterSearchString = d2.choices[0].text.substring(index + searchString.length + 1);
+    // setAnswer(valueAfterSearchString)
+    // console.log(valueAfterSearchString); // Output: "fox jumps over the lazy dog"
+
+    //save to db
+    const blog1 = await Questionnaire.create({question: valueAfterSearchString2, answer:  valueAfterSearchString });
+
+  }
+
+  res.json("success")
+  // res.json(d2['choices'][0]['text'])
+});
 
 module.exports = router;
